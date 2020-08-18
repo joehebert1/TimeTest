3e5c13b9312997e275646d42da10125a7464bd44
Joe Hebert <joehebert@aol.com>
2020-08-18 13:39:44 -0500

    First Commit

---
+++ MyTime.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTest
+{
+    class MyTime
+    {
+        #region Compare Time
+        private static bool IsFromLessThanTo(TimeSpan from, TimeSpan to)
+        {
+            //RETURNS for compare
+            //Value   Description
+            //- 1     t1 is shorter than t2.
+            //  0     t1 is equal to t2.
+            //  1     t1 is longer than t2.
+
+            return (TimeSpan.Compare(from, to) < 0); 
+        }
+
+        private static bool IsFromPositive (TimeSpan from)
+        {
+            TimeSpan newDay = new TimeSpan(0,0,0,0);
+            return (TimeSpan.Compare(from, newDay) >= 0);
+        }
+
+        private static bool IsToWithinDay(TimeSpan to)
+        {
+            TimeSpan fullDay = new TimeSpan(0, 23, 59, 0);
+            return (TimeSpan.Compare(to, fullDay) <= 0);
+        }
+
+        #endregion
+
+        #region Format Time
+        public static string ConvertTimeToHourMinute(TimeSpan ts)
+        {
+            return ts.ToString("hh\\:mm");
+        }
+        public static string ConvertTimeToHourMinuteAMPM(TimeSpan ts)
+        {
+            DateTime dt = DateTime.Today.Add(ts);
+            return dt.ToString("hh:mm tt");
+        }
+        #endregion
+
+        #region FROM Time
+        public static TimeSpan TryFromTimeAdd(TimeSpan from, TimeSpan to, TimeSpan value)
+        {
+            TimeSpan temp = new TimeSpan(from.Hours, from.Minutes,from.Seconds);
+
+            temp = temp.Add(value);
+
+            return IsFromLessThanTo(temp, to) ? temp : from;
+        }
+
+        public static TimeSpan TryFromTimeSubtract(TimeSpan from, TimeSpan to, TimeSpan value)
+        {
+            TimeSpan temp = new TimeSpan(from.Hours, from.Minutes, from.Seconds);
+
+            temp = temp.Subtract(value);
+
+            return IsFromPositive(temp) ? temp : from;
+
+        }
+
+        public static TimeSpan FromTimeAddHour(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneHour = new TimeSpan(1, 0, 0);
+            return MyTime.TryFromTimeAdd(from, to, oneHour);
+        }
+
+        public static TimeSpan FromTimeAddMinute(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneMinute = new TimeSpan(0, 1, 0);
+            return MyTime.TryFromTimeAdd(from, to, oneMinute);
+        }
+
+        public static TimeSpan FromTimeSubtractHour(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneHour = new TimeSpan(1, 0, 0);
+            return MyTime.TryFromTimeSubtract(from, to, oneHour);
+        }
+
+        public static TimeSpan FromTimeSubtractMinute(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneMinute = new TimeSpan(0, 1, 0);
+            return MyTime.TryFromTimeSubtract(from, to, oneMinute);
+        }
+        #endregion
+
+        #region TO Time
+        public static TimeSpan TryToTimeAdd(TimeSpan from, TimeSpan to, TimeSpan value)
+        {
+            TimeSpan temp = new TimeSpan(to.Hours, to.Minutes, to.Seconds);
+
+            temp = temp.Add(value);
+
+            return IsToWithinDay(temp) ? temp : to;
+
+        }
+
+        public static TimeSpan TryToTimeSubtract(TimeSpan from, TimeSpan to, TimeSpan value)
+        {
+            TimeSpan temp = new TimeSpan(to.Hours, to.Minutes, to.Seconds);
+
+            temp = temp.Subtract(value);
+
+            return IsFromLessThanTo(from, temp) ? temp : to;
+
+        }
+
+        public static TimeSpan ToTimeAddHour(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneHour = new TimeSpan(1, 0, 0);
+            return MyTime.TryToTimeAdd(from, to, oneHour);
+        }
+
+        public static TimeSpan ToTimeAddMinute(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneMinute = new TimeSpan(0, 1, 0);
+            return MyTime.TryToTimeAdd(from, to, oneMinute);
+        }
+
+        public static TimeSpan ToTimeSubtractHour(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneHour = new TimeSpan(1, 0, 0);
+            return MyTime.TryToTimeSubtract(from, to, oneHour);
+        }
+
+        public static TimeSpan ToTimeSubtractMinute(TimeSpan from, TimeSpan to)
+        {
+            TimeSpan oneMinute = new TimeSpan(0, 1, 0);
+            return MyTime.TryToTimeSubtract(from, to, oneMinute);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fromTime = "11:58";
+            string toTime = "13:00";
+
+            TimeSpan fromTS = TimeSpan.Parse(fromTime);
+            TimeSpan toTS = TimeSpan.Parse(toTime);
+            PrintOut(fromTS, toTS);
+
+            Console.WriteLine("FromTimeAddHour()\n");
+            fromTS = MyTime.FromTimeAddHour(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+
+            Console.WriteLine("FromTimeAddMinute()\n");
+            fromTS = MyTime.FromTimeAddMinute(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+
+            Console.WriteLine("FromTimeSubtractMinute()\n");
+            fromTS = MyTime.FromTimeSubtractMinute(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+
+            Console.WriteLine("FromTimeSubtractHour()\n");
+            fromTS = MyTime.FromTimeSubtractHour(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+
+
+            Console.WriteLine("ToTimeAddHour()\n");
+            toTS = MyTime.ToTimeAddHour(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+
+            Console.WriteLine("ToTimeAddMinute()\n");
+            toTS = MyTime.ToTimeAddMinute(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+
+            Console.WriteLine("ToTimeSubtractMinute()\n");
+            toTS = MyTime.ToTimeSubtractMinute(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+
+            Console.WriteLine("ToTimeSubtractHour()\n");
+            toTS = MyTime.ToTimeSubtractHour(fromTS, toTS);
+            PrintOut(fromTS, toTS);
+        }
+
+        static void PrintOut(TimeSpan from, TimeSpan to)
+        {
+            Console.WriteLine("FROM: " + MyTime.ConvertTimeToHourMinute(from) +
+                "\tTO: " + MyTime.ConvertTimeToHourMinute(to));
+
+            Console.WriteLine("FROM: " + MyTime.ConvertTimeToHourMinuteAMPM(from) +
+                "\tTO: " + MyTime.ConvertTimeToHourMinuteAMPM(to) + "\n");
+        }
+    }
+}
+/*
+FROM: 12:58     TO: 13:00
+FROM: 12:58 PM  TO: 01:00 PM
+
+FromTimeAddMinute()
+
+FROM: 12:59     TO: 13:00
+FROM: 12:59 PM  TO: 01:00 PM
+
+FromTimeSubtractMinute()
+
+FROM: 12:58     TO: 13:00
+FROM: 12:58 PM  TO: 01:00 PM
+
+FromTimeSubtractHour()
+
+FROM: 11:58     TO: 13:00
+FROM: 11:58 AM  TO: 01:00 PM
+
+ToTimeAddHour()
+
+FROM: 11:58     TO: 14:00
+FROM: 11:58 AM  TO: 02:00 PM
+
+ToTimeAddMinute()
+
+FROM: 11:58     TO: 14:01
+FROM: 11:58 AM  TO: 02:01 PM
+
+ToTimeSubtractMinute()
+
+FROM: 11:58     TO: 14:00
+FROM: 11:58 AM  TO: 02:00 PM
+
+ToTimeSubtractHour()
+
+FROM: 11:58     TO: 13:00
+FROM: 11:58 AM  TO: 01:00 PM
+
+Press any key to continue . . .
+
+*/
